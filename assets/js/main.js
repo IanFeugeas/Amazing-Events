69a6607769147f51efdd85d485bff0dfa0d63446
Ian Feugeas <ianfabricio1889@gmail.com>
2023-01-31 15:37:29 -0300

    cards agregadas a home por js

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const padrecards = document.getElementById("padrecards");
+let infocards = data.events;
+let divCards = "";
+
+for (let infocard of infocards) {
+  //   divCards += document.createElement("div");
+  //   divCards.className = "card";
+  divCards += `<div id="tarjetas" class="card">
+  <img
+    id="imgcard"
+    src= ${infocard.image}
+    class="card-img-top"
+    alt=""
+  />
+  <div class="card-body">
+    <h5 class="card-title"> ${infocard.name}</h5>
+    <p class="card-text">
+      ${infocard.description}
+    </p>
+    <div class="divprice">
+      <p><b>Price:</b> $ ${infocard.price}</p>
+      <a href="./details.html" class="btn btn-primary">Go details</a>
+    </div>
+  </div>
+</div>`;
+}
+padrecards.innerHTML = divCards;
